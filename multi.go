e3bc6796a081edd51fdde8d8dc0610dc998fc320
Matt Dainty <matt@bodgit-n-scarper.com>
2020-04-16 23:37:28 +0100

    Add MultiWriteCloser

---
+++ multi.go
@@ -0,0 +1,49 @@
+package plumbing
+
+import "io"
+
+type multiWriteCloser struct {
+	writeClosers []io.WriteCloser
+}
+
+func (t *multiWriteCloser) Write(p []byte) (n int, err error) {
+	for _, wc := range t.writeClosers {
+		n, err = wc.Write(p)
+		if err != nil {
+			return
+		}
+		if n != len(p) {
+			err = io.ErrShortWrite
+			return
+		}
+	}
+	return len(p), nil
+}
+
+func (t *multiWriteCloser) Close() (err error) {
+	for _, wc := range t.writeClosers {
+		err = wc.Close()
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+// MultiWriteCloser creates a writer that duplicates its writes to all the
+// provided writers, similar to the Unix tee(1) command.
+//
+// Each write is written to each listed writer, one at a time.
+// If a listed writer returns an error, that overall write operation
+// stops and returns the error; it does not continue down the list.
+func MultiWriteCloser(writeClosers ...io.WriteCloser) io.WriteCloser {
+	allWriteClosers := make([]io.WriteCloser, 0, len(writeClosers))
+	for _, wc := range writeClosers {
+		if mwc, ok := wc.(*multiWriteCloser); ok {
+			allWriteClosers = append(allWriteClosers, mwc.writeClosers...)
+		} else {
+			allWriteClosers = append(allWriteClosers, wc)
+		}
+	}
+	return &multiWriteCloser{allWriteClosers}
+}
---
+++ multi_test.go
@@ -0,0 +1,90 @@
+package plumbing
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var errClose = errors.New("error closing")
+
+type errorWriteCloser struct {
+	io.WriteCloser
+}
+
+func (errorWriteCloser) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (errorWriteCloser) Close() error {
+	return errClose
+}
+
+type partialWriter struct {
+	io.Writer
+}
+
+func (partialWriter) Write(p []byte) (n int, err error) {
+	return len(p) - 1, nil
+}
+
+func TestMultiWriteCloser(t *testing.T) {
+	tables := []struct {
+		name     string
+		writer   io.WriteCloser
+		n        int
+		writeErr error
+		closeErr error
+	}{
+		{
+			"success",
+			NopWriteCloser(new(bytes.Buffer)),
+			10,
+			nil,
+			nil,
+		},
+		{
+			"nested",
+			MultiWriteCloser(NopWriteCloser(new(bytes.Buffer))),
+			10,
+			nil,
+			nil,
+		},
+		{
+			"write",
+			NopWriteCloser(errorWriter{}),
+			0,
+			errWrite,
+			nil,
+		},
+		{
+			"close",
+			errorWriteCloser{},
+			10,
+			nil,
+			errClose,
+		},
+		{
+			"partial",
+			NopWriteCloser(partialWriter{}),
+			9,
+			io.ErrShortWrite,
+			nil,
+		},
+	}
+
+	for _, table := range tables {
+		t.Run(table.name, func(t *testing.T) {
+			dst := NopWriteCloser(new(bytes.Buffer))
+			w := MultiWriteCloser(table.writer, dst)
+			n, err := w.Write(in)
+			assert.Equal(t, table.n, n)
+			assert.Equal(t, table.writeErr, err)
+			err = w.Close()
+			assert.Equal(t, table.closeErr, err)
+		})
+	}
+}
